c9817c3e07ffa1e1678b59a248dbcad9033a858b
nicolas acard <nicolasacard@Ordinateur-portable-de-nicolas.local>
2025-07-12 15:45:35 +0200

    initial release

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ const article = require('./router/article')
 const auth = require('./router/auth')
 const event = require('./router/event')
 const upload = require('./router/faceup')
+const { PrismaClient } = require('@prisma/client') 
+const prisma = new PrismaClient()
 
 
 app.get('/', (req, res) => {
     res.json({message: 'hi from server'})
 })
+
+app.get('/users', async(req, res) => {
+    const users = await prisma.users.findMany()
+    res.json(users)
+})  
 
 app.use('/articles', article)
 app.use('/auth', auth)
